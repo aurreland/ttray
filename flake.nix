40d078b99679410401240ebdfca5e1e6de164aae
Aurélien Andreo <aurelien.andreo@proton.me>
2025-06-08 20:29:44 +0200

    fix: fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -1,47 +1,23 @@
 {
-  description = "Bevy Flake configuration";
+  description = "Ttray Flake configuration";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
   }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
     forEachSupportedSystem = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
         f {
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [rust-overlay.overlays.default self.overlays.default];
-          };
+          pkgs = import nixpkgs {inherit system;};
         });
   in {
-    overlays.default = final: prev: {
-      rustToolchain = let
-        rust = prev.rust-bin;
-      in
-        if builtins.pathExists ./rust-toolchain.toml
-        then rust.fromRustupToolchainFile ./rust-toolchain.toml
-        else if builtins.pathExists ./rust-toolchain
-        then rust.fromRustupToolchainFile ./rust-toolchain
-        else
-          rust.stable.latest.default.override {
-            extensions = ["rust-src" "rustfmt"];
-          };
-    };
-
     packages = forEachSupportedSystem ({pkgs}: {
       default = pkgs.rustPlatform.buildRustPackage {
         pname = "ttray";
-        version = "0.0.0";
+        version = "0.1.0";
 
         src = ./.;
         cargoLock = {
@@ -50,7 +26,7 @@
 
         meta = {
           description = "A Simple Tui for Tray Applications";
-          license = pkgs.lib.licenses.mit;
+          license = pkgs.lib.licenses.gpl3;
         };
       };
     });
@@ -58,11 +34,11 @@
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs; [
-          rustToolchain
+          cargo
           rust-analyzer
         ];
 
-        env.RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+        env.RUST_SRC_PATH = "${pkgs.rustc}/lib/rustlib/src/rust/library";
       };
     });
 
